65d7ab4dce3ab58fd7bbc7355a6fdb29489af070
Daniel Koziara <daniel@impresspro.pl>
2024-03-05 11:22:17 +0100

    first commit

---
+++ interfaces/IPULSEX.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.2;
+
+interface IPulseXFactory {
+   function createPair(address tokenA, address tokenB) external returns (address pair);
+}
+
+interface IPulseXRouter {
+   function factory() external pure returns (address);
+   function WPLS() external pure returns (address);
+   function addLiquidityETH(address token, uint amountTokenDesired, uint amountTokenMin, uint amountETHMin, address to, uint deadline) external payable returns (uint amountToken, uint amountETH, uint liquidity);
+   function swapExactTokensForETHSupportingFeeOnTransferTokens(uint amountIn, uint amountOutMin, address[] calldata path, address to, uint deadline) external;
+}
